938b338be421a825460fbf13bfd07bbafea69c5c
杨家兴 <yang.jia.xing@wowoohr.com>
2025-05-15 19:42:06 +0800

    a

--- sql生成系统/数据清洗/test.py
+++ sql生成系统/数据清洗/test.py
@@ -21,6 +21,21 @@ print(res.group(0), 'rrr222')
 
 for i in match:
     print(i, '匹配的内容xxx', i.span(), i.group(0))
+
+table = 'CREATE TABLE `bf_no_order_advance_payment_form_normal_payment` ('
+createTable = re.search(r'CREATE TABLE `(.)+`', table)
+print(createTable.group(0))
+
+table = ") ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='无订单垫付-付款单-无订单常规请款缴费-明细';"
+comment = re.search(r'COMMENT=\'(.)+\'', table)
+comment = re.finditer(r'`(.)+`(.)+COMMENT \'(.)+\'', table)
+match = list(comment)
+print(len(match), comment)
+
+
+
+
+
     
 
 
--- sql生成系统/数据清洗/数据清洗.py
+++ sql生成系统/数据清洗/数据清洗.py
@@ -1,33 +1,54 @@
+from collections import defaultdict
 import torch
 import re
-
-path = './数据清洗.txt'
+import json
 
 def clean_data(path):
-    result = []
+    result = {}
     with open(path, 'r', encoding='utf-8') as f:
         lines = f.read()
         segments = lines.split('\n\n')
-        print(segments)
+        # print(segments)
         # 处理一张表
         for segment in segments:
             if segment == '':
                 continue
-                # print(segment)
-                # re.search(r'powerful', text)
-            columnName = [] # 处理列名
-            tableName = [] # 处理表名
+            columnList = [] # 处理列名
+            tableName = '' # 处理表名
+            tableComment = '' # 处理表注释
             for text in segment.split('\n'):
                 createTable = re.search(r'CREATE TABLE `[^`]+`', text)
-                if createTable is not None:
-                    tableName.append(createTable.group(0).replace('`', ''))
+                if createTable != None:
+                    createTable = re.search(r'`(.)+`', createTable.group(0))
+                    tableName = createTable.group(0).replace('`', '')
                 else:
-                    # column = re.search(r'`[^`]+`', text)
                     match = re.finditer(r'`(.)+`(.)+COMMENT \'(.)+\'', text) # 匹配`column_name` xxx COMMENT 'xxx'
-                    if match is not None:
+                    if len(list(match)):
                         column = re.search(r'`(.)+`', text)
-                        # print(res.group(0), 'rrr')
-                        comment = re.search(r'\'(.)+\'', text)
+                        comment = re.search(r'COMMENT \'(.)+\'', text)
+                        if column is not None and comment is not None:
+                            column = column.group(0).replace('`', '')
+                            comment = comment.group(0).replace('COMMENT ', '').replace('\'', '')
+                            columnList.append({
+                                column: comment
+                            })
+                    else:
+                        tableComment = re.search(r'COMMENT=\'(.)+\';', text)
+                        if tableComment != None:
+                            tableComment = tableComment.group(0).replace('COMMENT=', '').replace('\'', '').replace(';', '')
+
+            result[tableName] = {}
+            result[tableName]['columns'] = columnList
+            result[tableName]['tableComment'] = tableComment
+
+    with open('database.json', 'w', encoding='utf-8') as f:
+        json.dump(result, f, ensure_ascii=False, indent=2)
+        f.close()
+    print(result)
+            
 
 
 
+if __name__ == '__main__':
+    path = './数据清洗.sql'
+    clean_data(path)
